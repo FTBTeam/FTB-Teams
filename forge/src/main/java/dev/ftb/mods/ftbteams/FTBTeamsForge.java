c9984daf6fe41c9d2efc600d3571848c250022ce
LatvianModder <latvianmodder@gmail.com>
2021-04-21 03:48:40 +0300

    Added kubejs.classfilter.txt

--- forge/src/main/java/dev/ftb/mods/ftbteams/FTBTeamsForge.java
+++ forge/src/main/java/dev/ftb/mods/ftbteams/FTBTeamsForge.java
@@ -1,5 +1,6 @@
 package dev.ftb.mods.ftbteams;
 
+import me.shedaniel.architectury.platform.forge.EventBuses;
 import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
@@ -11,6 +12,7 @@ import org.apache.commons.lang3.tuple.Pair;
 @Mod(FTBTeams.MOD_ID)
 public class FTBTeamsForge {
 	public FTBTeamsForge() {
+		EventBuses.registerModEventBus(FTBTeams.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
 		dev.ftb.mods.ftbteams.FTBTeams teams = new FTBTeams();
 		FMLJavaModLoadingContext.get().getModEventBus().<FMLCommonSetupEvent>addListener(event -> teams.setup());
 		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));

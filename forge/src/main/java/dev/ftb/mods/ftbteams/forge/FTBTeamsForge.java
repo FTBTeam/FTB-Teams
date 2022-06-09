460b5269e23c80ce8076faec39cb3ba360f4a0a5
Michael Hillcox <michaelhillcox@gmail.com>
2022-06-09 19:34:26 +0100

    fix: actually register argument types on forge

--- forge/src/main/java/dev/ftb/mods/ftbteams/forge/FTBTeamsForge.java
+++ forge/src/main/java/dev/ftb/mods/ftbteams/forge/FTBTeamsForge.java
@@ -7,6 +7,7 @@ import dev.ftb.mods.ftbteams.property.TeamPropertyArgument;
 import net.minecraft.commands.synchronization.ArgumentTypeInfo;
 import net.minecraft.commands.synchronization.ArgumentTypeInfos;
 import net.minecraft.core.Registry;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -23,9 +24,12 @@ public class FTBTeamsForge {
 	private static final RegistryObject<TeamPropertyArgument.Info> TEAM_PROPERTY_ARGUMENT = COMMAND_ARGUMENT_TYPES.register("team_property", () -> ArgumentTypeInfos.registerByClass(TeamPropertyArgument.class, new TeamPropertyArgument.Info()));
 
 	public FTBTeamsForge() {
-		EventBuses.registerModEventBus(FTBTeams.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
-		new FTBTeams();
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		EventBuses.registerModEventBus(FTBTeams.MOD_ID, modEventBus);
+		COMMAND_ARGUMENT_TYPES.register(modEventBus);
 
+		new FTBTeams();
+		
 //		FMLJavaModLoadingContext.get().getModEventBus().<FMLCommonSetupEvent>addListener(event -> teams.setup());
 		ModLoadingContext.get().registerExtensionPoint(DisplayTest.class, () -> new DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
 	}

b8721f3bb79094634de2c50cc8b08414cc7b0234
Des Herriott <des.herriott@gmail.com>
2023-06-29 08:57:40 +0100

    feat: notify client about command permissions
    forward-ported from 1.19 version

--- common/src/main/java/dev/ftb/mods/ftbteams/client/gui/CreatePartyButton.java
+++ common/src/main/java/dev/ftb/mods/ftbteams/client/gui/CreatePartyButton.java
@@ -7,25 +7,37 @@ import dev.ftb.mods.ftblibrary.ui.input.MouseButton;
 import dev.ftb.mods.ftblibrary.util.TooltipList;
 import dev.ftb.mods.ftbteams.FTBTeamsAPIImpl;
 import dev.ftb.mods.ftbteams.api.FTBTeamsAPI;
+import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 
 public class CreatePartyButton extends NordButton {
-	CreatePartyButton(Panel panel) {
-		super(panel, Component.translatable("ftbteams.create_party"), Icons.ADD);
+	private final boolean enabled;
+
+	CreatePartyButton(Panel panel, boolean enabled) {
+		super(panel, makeTitle(enabled), Icons.ADD);
+		this.enabled = enabled;
+	}
+
+	private static Component makeTitle(boolean enabled) {
+		MutableComponent c = Component.translatable("ftbteams.create_party");
+		return enabled ? c : c.withStyle(ChatFormatting.DARK_GRAY);
 	}
 
 	@Override
 	public void addMouseOverText(TooltipList list) {
-		list.translate("ftbteams.create_party.info");
+		list.translate(enabled ? "ftbteams.create_party.info" : "ftbteams.server_permissions_prevent");
 		list.maxWidth = 130;
 	}
 
 	@Override
 	public void onClicked(MouseButton button) {
-		if (FTBTeamsAPI.api().getCustomPartyCreationHandler() != null) {
-			FTBTeamsAPIImpl.INSTANCE.getCustomPartyCreationHandler().createParty(button);
-		} else {
-			new CreatePartyScreen().openGui();
+		if (enabled) {
+			if (FTBTeamsAPI.api().getCustomPartyCreationHandler() != null) {
+				FTBTeamsAPIImpl.INSTANCE.getCustomPartyCreationHandler().createParty(button);
+			} else {
+				new CreatePartyScreen().openGui();
+			}
 		}
 	}
 }
--- common/src/main/java/dev/ftb/mods/ftbteams/net/OpenMyTeamGUIMessage.java
+++ common/src/main/java/dev/ftb/mods/ftbteams/net/OpenMyTeamGUIMessage.java
@@ -5,18 +5,23 @@ import dev.architectury.networking.simple.BaseS2CMessage;
 import dev.architectury.networking.simple.MessageType;
 import dev.ftb.mods.ftbteams.api.property.TeamPropertyCollection;
 import dev.ftb.mods.ftbteams.client.FTBTeamsClient;
+import dev.ftb.mods.ftbteams.data.PlayerPermissions;
 import dev.ftb.mods.ftbteams.data.TeamPropertyCollectionImpl;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.server.level.ServerPlayer;
 
 public class OpenMyTeamGUIMessage extends BaseS2CMessage {
 	private final TeamPropertyCollection properties;
+	private final PlayerPermissions permissions;
 
-	public OpenMyTeamGUIMessage(TeamPropertyCollection properties) {
+	public OpenMyTeamGUIMessage(ServerPlayer player, TeamPropertyCollection properties) {
 		this.properties = properties;
+		this.permissions = new PlayerPermissions(player);
 	}
 
 	OpenMyTeamGUIMessage(FriendlyByteBuf buffer) {
 		this.properties = TeamPropertyCollectionImpl.fromNetwork(buffer);
+		this.permissions = PlayerPermissions.fromNetwork(buffer);
 	}
 
 	@Override
@@ -27,10 +32,11 @@ public class OpenMyTeamGUIMessage extends BaseS2CMessage {
 	@Override
 	public void write(FriendlyByteBuf buffer) {
 		properties.write(buffer);
+		permissions.toNetwork(buffer);
 	}
 
 	@Override
 	public void handle(NetworkManager.PacketContext context) {
-		FTBTeamsClient.openMyTeamGui(properties);
+		FTBTeamsClient.openMyTeamGui(properties, permissions);
 	}
 }

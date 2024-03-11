c98e40e139f2c59a71d89f95a1ddfe4f0647ef50
Des Herriott <des.herriott@gmail.com>
2024-03-11 22:01:17 +0000

    chore: add deprecation note to CustomPartyCreationHandler

--- common/src/main/java/dev/ftb/mods/ftbteams/api/CustomPartyCreationHandler.java
+++ common/src/main/java/dev/ftb/mods/ftbteams/api/CustomPartyCreationHandler.java
@@ -2,6 +2,9 @@ package dev.ftb.mods.ftbteams.api;
 
 import dev.ftb.mods.ftblibrary.ui.input.MouseButton;
 
+/**
+ * Deprecated: use {@link FTBTeamsAPI.API#setPartyCreationFromAPIOnly(boolean)}
+ */
 @FunctionalInterface
 @Deprecated
 public interface CustomPartyCreationHandler {

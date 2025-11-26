258cbba87b43aed5c291f879054dc2b2525aaece
Des Herriott <des.herriott@gmail.com>
2025-11-26 13:39:28 +0000

    fix: add missing class, oops

---
+++ common/src/main/java/dev/ftb/mods/ftbteams/api/property/BigIntegerProperty.java
@@ -0,0 +1,73 @@
+package dev.ftb.mods.ftbteams.api.property;
+
+import dev.ftb.mods.ftblibrary.config.ConfigFromString;
+import dev.ftb.mods.ftblibrary.config.ConfigGroup;
+import dev.ftb.mods.ftblibrary.config.ConfigValue;
+import net.minecraft.network.RegistryFriendlyByteBuf;
+import net.minecraft.resources.ResourceLocation;
+import org.jetbrains.annotations.Nullable;
+
+import java.math.BigInteger;
+import java.util.Optional;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public class BigIntegerProperty extends TeamProperty<BigInteger> {
+    protected BigIntegerProperty(ResourceLocation id, Supplier<BigInteger> def) {
+        super(id, def);
+    }
+
+    protected BigIntegerProperty(ResourceLocation id, BigInteger def) {
+        this(id, () -> def);
+    }
+
+    @Override
+    public TeamPropertyType<BigInteger> getType() {
+        return TeamPropertyType.BIG_INTEGER;
+    }
+
+    @Override
+    public Optional<BigInteger> fromString(String string) {
+        try {
+            return Optional.of(new BigInteger(string));
+        } catch (NumberFormatException e) {
+            return Optional.empty();
+        }
+    }
+
+    @Override
+    public void write(RegistryFriendlyByteBuf buf) {
+        buf.writeByteArray(getDefaultValue().toByteArray());
+    }
+
+    public static TeamProperty<BigInteger> fromNetwork(ResourceLocation id, RegistryFriendlyByteBuf buf) {
+        return new BigIntegerProperty(id, new BigInteger(buf.readByteArray()));
+    }
+
+    @Override
+    public BigInteger readValue(RegistryFriendlyByteBuf buf) {
+        return new BigInteger(buf.readByteArray());
+    }
+
+    @Override
+    public void writeValue(RegistryFriendlyByteBuf buf, BigInteger value) {
+        buf.writeByteArray(value.toByteArray());
+    }
+
+    @Override
+    public ConfigValue<?> config(ConfigGroup config, TeamPropertyValue<BigInteger> value) {
+        return config.add(id.getPath(), new BigIntegerConfig(), value.getValue(), value::setValue, getDefaultValue());
+    }
+
+    private static class BigIntegerConfig extends ConfigFromString<BigInteger> {
+        @Override
+        public boolean parse(@Nullable Consumer<BigInteger> consumer, String s) {
+            try {
+                BigInteger b = new BigInteger(s);
+                return okValue(consumer, b);
+            } catch (NumberFormatException ignored) {
+            }
+            return false;
+        }
+    }
+}
